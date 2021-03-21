3eb6b83197d89e47a83aefa93499ebd4a8556745
abeko <abeko6572@outlook.jp>
2021-03-21 17:41:39 +0900

    kadai

--- api/index.js
+++ api/index.js
@@ -1,11 +1,26 @@
-console.log('Hello World');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.json({
+        message: "Hello World!"
+    });
+});
+
+app.get('/api/get',function (req, res) {
+    res.json({
+        message: "Hello World!2"
+    });
+});
+
+app.post('/api/post',function (req, res) {
+    res.json({
+        message: "Hello World!3"
+    });
+    console.log(req.body);
 });
 
 (process.env.NOW_REGION) ? module.exports = app : app.listen(PORT); 
-
