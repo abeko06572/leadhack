7be097306d3b7781cc54e805b347c4d14f562b61
abeko <abeko6572@outlook.jp>
2021-04-11 16:35:44 +0900

    LINE

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
     res.json({
         message: "Hello World!"
     });
@@ -14,13 +14,12 @@ app.get('/api/get',function (req, res) {
     res.json({
         message: "Hello World!2"
     });
-});
+});*/
 
-app.post('/api/post',function (req, res) {
-    res.json({
-        message: "Hello World!3"
-    });
-    console.log(req.body);
+app.post('/',function (req, res) {
+    const data = req.body;
+    console.log('req.body',data);
+    res.send('api:OK');
 });
 
 (process.env.NOW_REGION) ? module.exports = app : app.listen(PORT); 
