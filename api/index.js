f493394ee46f5d83ce166b67f3eb72184f4c22b6
abeko <abeko6572@outlook.jp>
2021-04-11 22:58:40 +0900

    test01

--- api/index.js
+++ api/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const request = require('request');
+const accessToken = "22qm4wrQj2BqONMYsLxujMVPIDh9TLkpnUamTNS11i3PBwDtv2hK0hFD+aNkN1FHddvU5bxu39vX7FSXD9XwEey9sci6OINx0cphuUV7iyRF62cudwoHy264V44VrjcFTgYTmvYuHQH7oxbAA8rvoQdB04t89/1O/w1cDnyilFU=";
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const options = {
+    uri: 'https://api.line.me/v2/bot/message/${req.body.events[0].message.id}/content',
+    method: 'get',
+    headers: {
+        'Authorization': 'Bearer ' + accessToken,
+    },
+    encoding: null
+}
+
+request(options, function(error, response, body){
+    const buffer = new Buffer.from(body);
+    console.log(buffer);
+});
 
 /*app.get('/', function (req, res) {
     res.json({
