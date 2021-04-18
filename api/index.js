146bff61f565416468d1769530d96fbfb5254d5b
abeko <abeko6572@outlook.jp>
2021-04-18 14:54:48 +0900

    test02

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const request = require('request');
-const accessToken = "22qm4wrQj2BqONMYsLxujMVPIDh9TLkpnUamTNS11i3PBwDtv2hK0hFD+aNkN1FHddvU5bxu39vX7FSXD9XwEey9sci6OINx0cphuUV7iyRF62cudwoHy264V44VrjcFTgYTmvYuHQH7oxbAA8rvoQdB04t89/1O/w1cDnyilFU=";
+const accessToken = '22qm4wrQj2BqONMYsLxujMVPIDh9TLkpnUamTNS11i3PBwDtv2hK0hFD+aNkN1FHddvU5bxu39vX7FSXD9XwEey9sci6OINx0cphuUV7iyRF62cudwoHy264V44VrjcFTgYTmvYuHQH7oxbAA8rvoQdB04t89/1O/w1cDnyilFU=';
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
@@ -13,7 +13,7 @@ const options = {
         'Authorization': 'Bearer ' + accessToken,
     },
     encoding: null
-}
+};
 
 request(options, function(error, response, body){
     const buffer = new Buffer.from(body);
